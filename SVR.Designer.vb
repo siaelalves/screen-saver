0e8be290b6150dad6ee434a66eca85e5090d7a10
Siael Alves <32615727+siaelalves@users.noreply.github.com>
2021-07-15 13:13:27 -0300

    Upload inicial

---
+++ SVR.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SRV
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblTm = New System.Windows.Forms.Label()
+        Me.tmHr = New System.Windows.Forms.Timer(Me.components)
+        Me.tmPic = New System.Windows.Forms.Timer(Me.components)
+        Me.tmMs = New System.Windows.Forms.Timer(Me.components)
+        Me.dlgOp = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'lblTm
+        '
+        Me.lblTm.AutoSize = True
+        Me.lblTm.BackColor = System.Drawing.Color.Transparent
+        Me.lblTm.Font = New System.Drawing.Font("Century751 BT", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTm.ForeColor = System.Drawing.Color.White
+        Me.lblTm.Location = New System.Drawing.Point(-11, 295)
+        Me.lblTm.Name = "lblTm"
+        Me.lblTm.Size = New System.Drawing.Size(349, 115)
+        Me.lblTm.TabIndex = 0
+        Me.lblTm.Text = "Tempo"
+        Me.lblTm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'tmHr
+        '
+        Me.tmHr.Interval = 1000
+        '
+        'tmPic
+        '
+        Me.tmPic.Interval = 30000
+        '
+        'tmMs
+        '
+        Me.tmMs.Enabled = True
+        Me.tmMs.Interval = 30000
+        '
+        'dlgOp
+        '
+        Me.dlgOp.Multiselect = True
+        Me.dlgOp.RestoreDirectory = True
+        Me.dlgOp.Title = "Adicionar Fotos"
+        '
+        'SRV
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(324, 415)
+        Me.Controls.Add(Me.lblTm)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.KeyPreview = True
+        Me.Name = "SRV"
+        Me.Text = "Proteção de tela"
+        Me.TopMost = True
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblTm As System.Windows.Forms.Label
+    Friend WithEvents tmHr As System.Windows.Forms.Timer
+    Friend WithEvents tmPic As System.Windows.Forms.Timer
+    Friend WithEvents tmMs As System.Windows.Forms.Timer
+    Friend WithEvents dlgOp As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ SVR.vb
@@ -0,0 +1,96 @@
+﻿Public Class SRV
+    '
+    Shared tm As String = DateAndTime.Now.ToString("hh:mm:ss")
+    Shared dt As String = DateAndTime.Now.ToString("d' de 'MMMM' de 'yyyy")
+    Shared pics As New Collection()
+    Shared cript As New Segurança.Criptografia()
+    '
+    Private Sub SRV_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        On Error Resume Next
+        Dim d() As String = IO.File.ReadAllLines("data.ini", System.Text.Encoding.Default)
+        '
+        For i = 0 To UBound(d)
+            pics.Add(cript.Descriptografar(d(i)))
+        Next
+        '
+        lblTm.Text = dt & Chr(13) & tm
+        '
+        tmHr.Start()
+        tmPic.Start()
+        tmMs.Start()
+        '
+        lblTm.Location = New Drawing.Point(Int(Me.Size.Width / 2) - (lblTm.Width / 2), _
+                                            Me.Size.Height - (lblTm.Height * 1))
+        '
+        Dim rn As New Random()
+        On Error Resume Next
+        Me.BackgroundImage = Bitmap.FromFile(pics(rn.Next(0, pics.Count - 1)))
+        '
+    End Sub
+
+    Private Sub tmHr_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmHr.Tick
+        '
+        tm = DateAndTime.Now.ToString("hh:mm:ss")
+        lblTm.Text = dt & Chr(13) & tm
+        '
+    End Sub
+
+    Private Sub tmPic_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmPic.Tick
+        '
+        Dim rn As New Random()
+        '
+        If pics.Count > 0 Then
+            On Error Resume Next
+            Me.BackgroundImage = Bitmap.FromFile(pics(rn.Next(0, pics.Count + 1)))
+        ElseIf pics.Count = 0 Then
+            Me.BackgroundImage = Nothing
+        End If
+        '
+    End Sub
+
+    Private Sub SRV_Click(sender As System.Object, e As System.EventArgs) Handles MyBase.Click
+        '
+        Me.Close()
+        '
+    End Sub
+
+    Private Sub SRV_PreviewKeyDown(sender As System.Object, e As System.Windows.Forms.PreviewKeyDownEventArgs) Handles MyBase.PreviewKeyDown
+        '
+        If e.KeyData = Keys.F2 Then
+            '
+            dlgOp.Filter = "Fotos (*.jpg, *jpeg, *.bmp, *.gif, *.png)|*.jpg;*.jpeg;*.bmp;*.gif;*.png"
+            '
+            If dlgOp.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                On Error Resume Next
+                FileOpen(1, "data.ini", OpenMode.Append)
+                For Each f In dlgOp.FileNames
+                    PrintLine(1, cript.Criptografar(f))
+                    pics.Add(f)
+                Next
+                FileClose(1)
+            End If
+        '
+        ElseIf e.KeyData = Keys.F3 Then
+            '
+            If MsgBox("Tem certeza que deseja excluir todas as fotos do Slide Show?", _
+                      MsgBoxStyle.YesNo + MsgBoxStyle.Exclamation, "Exclusão") = MsgBoxResult.Yes Then
+                On Error Resume Next
+                IO.File.Delete("data.ini")
+                pics.Clear()
+                MsgBox("Todas as fotos foram excluídas!", MsgBoxStyle.Information, "Confirmação")
+            End If
+            '
+        Else
+            '
+            Me.Close()
+            '
+        End If
+        '
+    End Sub
+
+    Private Sub SRV_Paint(sender As System.Object, e As System.Windows.Forms.PaintEventArgs) Handles MyBase.Paint
+        '
+        Me.Update()
+        '
+    End Sub
+End Class
